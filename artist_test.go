d8472d3692d0deffad3fee8389f2cf92dddb4494
Michael Wendland <accounts@michiwend.com>
2014-11-16 21:12:02 +0100

    adds testing for Artist lookup

--- artist_test.go
+++ artist_test.go
@@ -100,7 +100,97 @@ func TestSearchArtist(t *testing.T) {
 
 	if !reflect.DeepEqual(*returned, want) {
 		t.Error(requestDiff(&want, returned))
-
-		//fmt.Print(pretty.Diff(want, *returned))
 	}
+}
+
+func TestLookupArtist(t *testing.T) {
+
+	want := Artist{
+		ID:             "10adbe5e-a2c0-4bf3-8249-2b4cbf6e6ca8",
+		Type:           "Group",
+		Name:           "Massive Attack",
+		Disambiguation: "",
+		SortName:       "Massive Attack",
+		CountryCode:    "",
+		Area: Area{
+			ID:       "40d758a4-b7c2-40f3-b439-5efbd2a3b038",
+			Name:     "Bristol",
+			SortName: "Bristol",
+			ISO31662Codes: []ISO31662Code{
+				"GB-BST",
+			},
+		},
+		BeginArea: Area{
+			ID:       "40d758a4-b7c2-40f3-b439-5efbd2a3b038",
+			Name:     "Bristol",
+			SortName: "Bristol",
+			ISO31662Codes: []ISO31662Code{
+				"GB-BST",
+			},
+		},
+		Lifespan: Lifespan{
+			Ended: false,
+			Begin: BrainzTime{time.Date(1987, 1, 1, 0, 0, 0, 0, time.UTC)},
+			End:   BrainzTime{time.Time{}},
+		},
+		Relations: TargetRelationsMap{
+			"artist": []Relation{
+				&ArtistRelation{
+					RelationAbstract: RelationAbstract{
+						TypeID:    "5be4c609-9afa-4ea0-910b-12ffb71e3821",
+						Type:      "member of band",
+						Target:    "54912e02-166c-49fe-ba95-cd77ef182390",
+						Direction: "backward",
+						Begin:     BrainzTime{time.Date(1987, 1, 1, 0, 0, 0, 0, time.UTC)},
+						End:       BrainzTime{time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)},
+						Ended:     true,
+					},
+					// TODO Attribute list
+					Artist: Artist{
+						ID:             "54912e02-166c-49fe-ba95-cd77ef182390",
+						Name:           "Mushroom",
+						SortName:       "Mushroom",
+						Disambiguation: "Andrew Vowles, member of Massive Attack",
+					},
+				},
+			},
+			"release": []Relation{
+				&ReleaseRelation{
+					RelationAbstract: RelationAbstract{
+						TypeID: "307e95dd-88b5-419b-8223-b146d4a0d439",
+						Type:   "design/illustration",
+						Target: "07832b54-8266-47d5-bb0e-62c7f2cf5da5",
+					},
+					Release: Release{
+						ID:          "07832b54-8266-47d5-bb0e-62c7f2cf5da5",
+						Title:       "Protection",
+						Quality:     "normal",
+						Date:        BrainzTime{time.Date(1995, 1, 24, 0, 0, 0, 0, time.UTC)},
+						CountryCode: "US",
+						Barcode:     "724383988327",
+					},
+				},
+			},
+		},
+	}
+
+	setupHTTPTesting()
+	defer server.Close()
+	serveTestFile(
+		"/artist/10adbe5e-a2c0-4bf3-8249-2b4cbf6e6ca8",
+		"LookupArtist.xml", t)
+
+	returned, err := client.LookupArtist(
+		"10adbe5e-a2c0-4bf3-8249-2b4cbf6e6ca8",
+		"artist-rels",
+		"release-rels")
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if !reflect.DeepEqual(*returned, want) {
+		t.Error(requestDiff(&want, returned))
+	}
+
 }
