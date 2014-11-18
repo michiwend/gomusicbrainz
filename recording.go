c7eea7dc1e10f9f3c3705096996b58bbbec3e9f2
Michael Wendland <accounts@michiwend.com>
2014-11-18 22:59:04 +0100

    implemented Recording

--- recording.go
+++ recording.go
@@ -25,15 +25,89 @@
 
 package gomusicbrainz
 
+import "encoding/xml"
+
 type Recording struct {
-	//TODO implement
+	ID             MBID         `xml:"id,attr"`
+	Title          string       `xml:"title"`
+	Length         int          `xml:"length"`
+	Disambiguation string       `xml:"disambiguation"`
+	ArtistCredit   ArtistCredit `xml:"artist-credit"`
+
+	// TODO add refs
 }
 
+func (mbe *Recording) lookupResult() interface{} {
+	var res struct {
+		XMLName xml.Name   `xml:"metadata"`
+		Ptr     *Recording `xml:"recording"`
+	}
+	res.Ptr = mbe
+	return &res
+}
+
+func (mbe *Recording) apiEndpoint() string {
+	return "/recording"
+}
+
+func (mbe *Recording) Id() MBID {
+	return mbe.ID
+}
+
+// LookupRecording performs an recording lookup request for the given MBID.
+func (c *WS2Client) LookupRecording(id MBID, inc ...string) (*Recording, error) {
+	a := &Recording{ID: id}
+	err := c.Lookup(a, inc...)
+
+	return a, err
+}
+
+// SearchRecording queries MusicBrainz´ Search Server for Recordings.
+// With no fields specified searchTerm searches the recording field only. For a
+// list of all valid fields visit
+// http://musicbrainz.org/doc/Development/XML_Web_Service/Version_2/Search#Recording
 func (c *WS2Client) SearchRecording(searchTerm string, limit, offset int) (*RecordingSearchResponse, error) {
-	//TODO implement
-	return nil, nil
+
+	result := recordingListResult{}
+	err := c.searchRequest("/recording", &result, searchTerm, limit, offset)
+
+	rsp := RecordingSearchResponse{}
+	rsp.WS2ListResponse = result.RecordingList.WS2ListResponse
+	rsp.Scores = make(ScoreMap)
+
+	for i, v := range result.RecordingList.Recordings {
+		rsp.Recordings = append(rsp.Recordings, v.Recording)
+		rsp.Scores[rsp.Recordings[i]] = v.Score
+	}
+
+	return &rsp, err
 }
 
+// RecordingSearchResponse is the response type returned by the SearchRecording
+// method.
 type RecordingSearchResponse struct {
-	//TODO implement
+	WS2ListResponse
+	Recordings []*Recording
+	Scores     ScoreMap
+}
+
+// ResultsWithScore returns a slice of Recordings with a specific score.
+func (r *RecordingSearchResponse) ResultsWithScore(score int) []*Recording {
+	var res []*Recording
+	for k, v := range r.Scores {
+		if v == score {
+			res = append(res, k.(*Recording))
+		}
+	}
+	return res
+}
+
+type recordingListResult struct {
+	RecordingList struct {
+		WS2ListResponse
+		Recordings []struct {
+			*Recording
+			Score int `xml:"http://musicbrainz.org/ns/ext#-2.0 score,attr"`
+		} `xml:"recording"`
+	} `xml:"recording-list"`
 }
---
+++ recording_test.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2014 Michael Wendland
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ *	Authors:
+ * 		Michael Wendland <michael@michiwend.com>
+ */
+
+package gomusicbrainz
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestSearchRecording(t *testing.T) {
+
+	want := RecordingSearchResponse{
+		WS2ListResponse: WS2ListResponse{
+			Count:  1,
+			Offset: 0,
+		},
+		Recordings: []*Recording{
+			{
+				ID:     "07339604-c19c-4efe-9195-f9c3b127a458",
+				Title:  "Fred",
+				Length: 473000,
+				ArtistCredit: ArtistCredit{
+					NameCredit{
+						Artist{
+							ID:       "695e75b5-c6db-43ee-abeb-2f3e50d96c3e",
+							Name:     "Imperiet",
+							SortName: "Imperiet",
+						},
+					},
+				},
+				//TODO add missing fields
+			},
+		},
+	}
+
+	setupHTTPTesting()
+	defer server.Close()
+	serveTestFile("/recording", "SearchRecording.xml", t)
+
+	returned, err := client.SearchRecording("Fred", -1, -1)
+	if err != nil {
+		t.Error(err)
+	}
+
+	want.Scores = ScoreMap{
+		returned.Recordings[0]: 100,
+	}
+
+	if !reflect.DeepEqual(*returned, want) {
+		t.Error(requestDiff(&want, returned))
+	}
+}
